b5d4fb6bcb462e0979dd24f010156a9134e04f38
khushisen <khushisen9001@gmail.com>
2025-04-19 00:38:57 +0530

    adding hotel booking

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#home/ models.py
+
+# other models
+class HotelBooking(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete = models.CASCADE , related_name="bookings" )
+    booking_user = models.ForeignKey(HotelUser, on_delete = models.CASCADE , )
+    booking_start_date = models.DateField()
+    booking_end_date = models.DateField()
+    price = models.FloatField()
