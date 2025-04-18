408bde49394ec2f18be515640c854fb5bf94c811
khushisen <khushisen9001@gmail.com>
2025-04-18 17:25:22 +0530

    added authentication for vendors

--- accounts/models.py
+++ accounts/models.py
@@ -9,15 +9,20 @@ class HotelUser(User):
     otp = models.CharField(max_length=10,null=True,blank=True)
     is_verified = models.BooleanField(default=False)
     
+    class Meta:
+        db_table = "hotel_user"
     
 class HotelVendor(User):
     phone_number = models.CharField(unique=True,max_length=100)
     profile_picture = models.ImageField(upload_to="profile")
     email_token = models.CharField(max_length = 100 ,null = True , blank=True)
     otp = models.CharField(max_length = 10 , null = True , blank = True)
-
+    business_name = models.CharField(max_length=100)
+    
     is_verified = models.BooleanField(default = False)
     
+    class Meta:
+        db_table = "hotel_vendors"
     
 class Ameneties(models.Model):
     name=models.CharField(max_length=1000)
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('send_otp/<email>',views.send_otp,name='send_otp'),
     path('verify-otp/<email>/' , views.verify_otp, name='verify_otp'),
     
-    path('verify-account/<token>/', views.verify_email_token, name="verify_email_token")
-
+    path('verify-account/<token>/', views.verify_email_token, name="verify_email_token"),
+    path('login-vendor/',views.login_vendor,name='login_vendor'),
+    path('register-vendor/',views.register_vendor,name='register_vendor'),
+    path('dashboard/',views.dashboard,name="dashboard"),
+    
+    
 ]
